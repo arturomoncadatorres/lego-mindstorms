c60c6315c74aac1f7f9898a9ba6de3abd8f4ff7a
Arturo Moncada-Torres <arturomoncadatorres@gmail.com>
2021-01-04 16:05:30 +0100

    feat: Added source code link to notebooks

--- base/charlie/here_i_go.py
+++ base/charlie/here_i_go.py
@@ -22,6 +22,9 @@
 #
 # <img src="./multimedia/charlie.png" width="50%" align="center">
 #
+# # Source code
+# You can find the code in the accompanying [`.py` file](https://github.com/arturomoncadatorres/lego-mindstorms/blob/main/base/charlie/here_i_go.py). To get it running, simply copy and paste it in a new Mindstorms project.
+#
 # # Imports
 
 # %%
